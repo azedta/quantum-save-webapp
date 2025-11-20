4eae2fb39f60d7c8a62290af76e035c457ec23f1
Aziz Tazrout <aztazrout@gmail.com>
2025-11-19 19:21:13 -0500

    Set up React Project Structure with core pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Income from "./pages/Income.jsx";
+import Expense from "./pages/Expense.jsx";
+import Category from "./pages/Category.jsx";
+import Filter from "./pages/Filter.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import {Toaster} from "react-hot-toast";
+
 const App = () => {
 
     return (
-        <div className="text-3xl font-bold underline"> Quantum Save</div>
+        <>
+            <Toaster/>
+            <BrowserRouter>
+            <Routes>
+                <Route path="/dashboard" element={<Home/>} />
+                <Route path="/income" element={<Income/>} />
+                <Route path="/expense" element={<Expense/>} />
+                <Route path="/category" element={<Category/>} />
+                <Route path="/filter" element={<Filter/>} />
+                <Route path="/login" element={<Login/>} />
+                <Route path="/signup" element={<Signup/>} />
+            </Routes>
+        </BrowserRouter>
+        </>
     )
 }
 
---
+++ src/assets/assets.js
@@ -0,0 +1,9 @@
+import quantum_save_brand from "./quantum-save-brand.png";
+import quantum_save_icon from "./quantum-save-icon.png";
+import quantum_save_logo from "./quantum-save-logo.png";
+
+export const assets = {
+    quantum_save_brand,
+    quantum_save_icon,
+    quantum_save_logo,
+}
